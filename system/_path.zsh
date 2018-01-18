1a4c90029c9d22e3264a3dc78cd5e54470e315ce
Jared Murphy <jlmurphy.sa@gmail.com>
2018-01-18 09:41:14 +0200

    Support for nvm and use vscode-insiders as default editor

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,4 +1,6 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/.dotfiles/bin:$PATH"
 export PATH=~/.composer/vendor/bin:$PATH
 export PATH=/usr/bin/python3:$PATH
+export NVM_DIR="$HOME/.nvm"
+source "/usr/local/opt/nvm/nvm.sh"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
