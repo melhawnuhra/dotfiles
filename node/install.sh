32862aa63310559c4e424378dd2799c5501c4d2b
Jared Murphy <jlmurphy.sa@gmail.com>
2018-04-22 21:09:35 +0200

    remember to add npm global packages to node/install.sh in future...

--- node/install.sh
+++ node/install.sh
@@ -2,3 +2,13 @@ if test ! $(which spoof)
 then
   sudo npm install spoof -g
 fi
+
+if test ! $(which gtop)
+then
+  sudo npm install gtop -g
+fi
+
+if test ! $(which fd)
+then
+  sudo npm install fd -g
+fi
