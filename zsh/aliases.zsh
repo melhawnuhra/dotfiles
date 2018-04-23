84c7df6d32f5398821bde5285639fe4c71bd5b13
Jared Murphy <jlmurphy.sa@gmail.com>
2018-04-23 02:05:13 +0200

    set vim to default editor, fix recursive alias definition

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,6 +2,7 @@ alias reload='. ~/.zshrc'   # Re-source .zshrc
 alias c='clear' # Good 'ol Clear Screen command
 alias cats='pygmentize -g'		# Syntax-highlighted cat
 alias cpwd='pwd|tr -d "\n"|pbcopy'	# Copy pwd path
+alias code='code-insiders'
 
 # Fuck alias
 if $(which thefuck &>/dev/null)
@@ -10,8 +11,9 @@ then
 fi
 
 # Custom cd
-chd() {
-  cd $1;
-  ls;
+unalias cd
+function chd() {
+  cd $1 && ls
 }
+
 alias cd="chd"
