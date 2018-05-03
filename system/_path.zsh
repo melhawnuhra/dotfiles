c780199ff37701f7fd2620d77e1f4db3ade903db
Jared Murphy <jlmurphy.sa@gmail.com>
2018-05-03 12:11:12 +0200

    fix nvm out-of-the-box setup

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,5 +1,4 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$HOME/dotfiles/bin:$PATH"
 export PATH=~/.composer/vendor/bin:$PATH
 export PATH=/usr/bin/python3:$PATH
-export NVM_DIR="$HOME/.nvm"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
