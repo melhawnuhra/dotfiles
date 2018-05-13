2f1398fc2bed039c9354693bb7affbc221eb5868
Jared Murphy <jlmurphy.sa@gmail.com>
2018-05-13 13:01:48 +0200

    Better tmux mouse mode, fix pwd preservation for tmux splits, remove deprecated fluff, simplified git diff alias and allow support for --staged option

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -27,7 +27,7 @@ alias gl='git log --oneline'
 alias gh="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 
 # Remove `+` and `-` from start of diff lines; just rely upon color.
-alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
+alias gd='git diff --color-words'
 
 # Pull / push
 alias gu='git push'
