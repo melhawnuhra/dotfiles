0eee2d61d39fa57576f766a54bb624016a3aedf6
Jared Murphy <jlmurphy.sa@gmail.com>
2018-08-16 12:52:45 +0200

    port jobs prompt to fish

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -3,7 +3,7 @@ set fish_prompt_pwd_dir_length 0
 # Git prompt
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch red
+set __fish_git_prompt_color_branch magenta
 set __fish_git_prompt_color_dirtystate FCBC47
 set __fish_git_prompt_color_stagedstate green
 set __fish_git_prompt_color_upstream cyan
@@ -16,6 +16,18 @@ set __fish_git_prompt_char_upstream_equal ''
 set __fish_git_prompt_char_upstream_ahead '⇡'
 set __fish_git_prompt_char_upstream_behind '⇣'
 set __fish_git_prompt_char_upstream_diverged '⇡⇣'
+
+function _fish_jobs_prompt
+  set -l jobs_amount (jobs | wc -l | xargs)
+
+  if test $jobs_amount -eq 0
+    echo ""
+  else if test $jobs_amount -eq 1
+      echo "✦ "
+  else
+    echo "$jobs_amount✦ "
+  end
+end
 
 function _print_in_color
   set -l string $argv[1]
@@ -37,9 +49,11 @@ end
 function fish_prompt
   set -l last_status $status
 
-  _print_in_color "\n"(prompt_pwd) blue
+  _print_in_color "\n"(prompt_pwd) cyan
 
   __fish_git_prompt " %s"
 
-  _print_in_color "\n❯ " (_prompt_color_for_status $last_status)
+  printf "\n"
+  _print_in_color (_fish_jobs_prompt) "yellow"
+  _print_in_color "❯ " (_prompt_color_for_status $last_status)
 end
