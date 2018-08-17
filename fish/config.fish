3824d2e298403be287cc8cc80079a4e44d05220e
Jared Murphy <jlmurphy.sa@gmail.com>
2018-08-17 02:31:54 +0200

    cleaned up tmux config, set correct TERM options with fish and tmux, some theming

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,12 @@
 
 set DOTFILES_HOME "$HOME/dotfiles"
 
-set -x TERM screen-256color
+if [ -n "$TMUX" ]
+  set -x TERM screen-256color
+else
+  set -x TERM xterm-256color
+end
+
 set -xU EXA_COLORS "di=34:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34:su=0:sg=0:tw=0:ow=0:"
 set LSCOLORS "exfxcxdxbxegedabagacad"
 set CLICOLOR "true"
