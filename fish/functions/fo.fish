c80519fdab59986d4eb0a42eb9e872992530bcfc
Jared Murphy <jlmurphy.sa@gmail.com>
2019-07-30 16:17:06 +0200

    git: faster checkout of recent branches

--- fish/functions/fo.fish
+++ fish/functions/fo.fish
@@ -1,3 +1,8 @@
 function fo -d "Fuzzy-find and checkout a branch"
-  git branch --all | grep -v HEAD | string trim | fzf-tmux | read -l result; and git checkout "$result"
+  # git branch --sort='authordate:iso8601' --format=' %(authordate:relative)%09%(refname:short)' | tac | fzf-tmux | read -l result; and git checkout "$result"
+  git branch --sort='authordate:iso8601' --format=' %(authordate:relative)%09%(refname:short)' | tac | fzf-tmux |  sed 's/.*ago[[:space:]]*//' | read -l result; and git checkout "$result"
 end
+
+# function foo -d "Fuzzy-find and checkout a branch"
+#   | grep -v HEAD | string trim | fzf-tmux | read -l result; and git checkout "$result"
+# end
