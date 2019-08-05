02881282f19169864d1959c8d255c546681f748c
Jared Murphy <jlmurphy.sa@gmail.com>
2019-08-05 20:48:22 +0200

    git: update abbrs - add gbd, gbm, gob

--- git/aliases.fish
+++ git/aliases.fish
@@ -9,34 +9,50 @@ if not set -q git_abbrs_initialized
   echo -n Setting Git abbreviations...
 
   abbr g 'git'
+
   abbr ga 'git add'
   abbr gaa 'git add -A'
   abbr gacp 'git add -A; and git-amend; and git push origin +master'
   abbr gap 'git add -p'
+
   abbr gb 'git branch'
+  abbr gbd 'git branch -D'
+  abbr gbm 'git branch -m'
+
   abbr gbl 'git blame'
+
   abbr gc 'git commit -m'
   abbr gca 'git-amend'
+
   abbr go 'git checkout'
+  abbr gob 'git checkout -b'
+
   abbr gcp 'git cherry-pick'
+
   abbr gd 'git diff --color-words'
   abbr gds 'git diff --color-words --staged'
+
   abbr gf 'git fetch'
   abbr gl 'git log'
   abbr gm 'git merge'
+
   abbr gpo 'git push origin (git rev-parse --abbrev-ref HEAD)'
   abbr gpo+ "git push origin +(git rev-parse --abbrev-ref HEAD)"
   abbr gpod 'git push origin dev'
   abbr gpom 'git push origin master'
   abbr gpo+d 'git push origin +dev'
   abbr gpo+m 'git push origin +master'
+
   abbr gpl 'git pull'
   abbr gplr 'git pull --rebase'
+
   abbr gr 'git reset'
   abbr gs 'git status -sb'
+
   abbr gst 'git stash'
   abbr gstp 'git stash pop'
   abbr gstl 'git stash list'
+
   abbr gw 'git-wtf'
   abbr st 'open -a SourceTree .'
 
